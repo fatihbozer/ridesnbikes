125566d565cf3318b8997e2b903c714464ca0f73
fatihbozer <fatih.bozer@outlook.de>
2023-11-09 14:33:23 +0100

    bikechip klasse

--- lib/rides_widgets/rides_widgets.dart
+++ lib/rides_widgets/rides_widgets.dart
@@ -9,3 +9,31 @@ Widget buildPostPicture() => const CircleAvatar(
       radius: 30.0,
       backgroundImage: NetworkImage('https://images.unsplash.com/photo-1558981806-ec527fa84c39?auto=format&fit=crop&q=80&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'),
     );
+
+class BikeChip extends StatefulWidget {
+  final String text;
+
+  const BikeChip({super.key, required this.text});
+
+  @override
+  State<BikeChip> createState() => _BikeChipsState();
+}
+
+class _BikeChipsState extends State<BikeChip> {
+  bool isSelected = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FilterChip(
+        label: Text(widget.text),
+        selected: isSelected,
+        onSelected: (bool value) {
+          setState(() {
+            isSelected = !isSelected;
+          });
+        },
+      ),
+    );
+  }
+}

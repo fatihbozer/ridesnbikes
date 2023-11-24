f58284838436074794fb1c3dca2fdcafef6ade81
fatihbozer <fatih.bozer@outlook.de>
2023-11-24 16:17:32 +0100

    beitrag erstellen screen bearbeitet

--- lib/rides_screens/post_screen.dart
+++ lib/rides_screens/post_screen.dart
@@ -5,8 +5,65 @@ class EditScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Text('Beitrag erstellen'),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Center(
+            child: Container(
+              height: 400,
+              width: 400,
+              color: Colors.black,
+              child: const Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Kamera',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 16),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.camera_alt_outlined),
+                iconSize: 50,
+              ),
+            ],
+          ),
+          const SizedBox(height: 16),
+          Expanded(
+            flex: 1,
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(width: 180, height: 180, color: Colors.black),
+                      Container(width: 180, height: 180, color: Colors.black),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Container(width: 180, height: 180, color: Colors.black),
+                      Container(width: 180, height: 180, color: Colors.black),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }

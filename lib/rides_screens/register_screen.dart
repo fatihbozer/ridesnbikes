271efaad096ef6405ee58a72fef61ec3942c7b55
fatihbozer <fatih.bozer@outlook.de>
2024-01-15 16:10:52 +0100

    usernamen mindestlänge beim registrieren eingefügt

--- lib/rides_screens/register_screen.dart
+++ lib/rides_screens/register_screen.dart
@@ -33,8 +33,10 @@ class _RegisterPageState extends State<RegisterPage> {
 
     if (passwordController.text != confirmPwController.text) {
       Navigator.pop(context);
-
       displayMessageToUser('Passwords dont match!', context);
+    } else if (usernameController.text.length < 3) {
+      Navigator.pop(context);
+      displayMessageToUser('Username must be at least 3 characters long!', context);
     } else {
       try {
         UserCredential? userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(

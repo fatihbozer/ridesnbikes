0e64d8dbd29977f313702870de0ba366702e8f6f
fatihbozer <fatih.bozer@outlook.de>
2024-02-06 20:17:17 +0100

    beiträge werden im search angezeigt

--- lib/rnb_Screens/SearchScreen/search_screen.dart
+++ lib/rnb_Screens/SearchScreen/search_screen.dart
@@ -29,59 +29,48 @@ class SearchScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView(
-        children: const [
-          MyPictures(),
-          MyPictures(),
-          MyPictures(),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: FutureBuilder(
+          future: FirebaseFirestore.instance.collection('posts').get(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                crossAxisSpacing: 8.0,
+                mainAxisSpacing: 8.0,
+              ),
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                var post = snapshot.data!.docs[index].data();
+                return GestureDetector(
+                  onTap: () {
+                    // Hier kannst du die Logik für die Behandlung eines Klicks auf ein Bild hinzufügen.
+                  },
+                  child: CachedNetworkImage(
+                    imageUrl: post['postUrl'],
+                    placeholder: (context, url) => const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => const Icon(Icons.error),
+                    fit: BoxFit.cover,
+                  ),
+                );
+              },
+            );
+          },
+        ),
       ),
     );
   }
 }
 
 class CustomSearchDelegate extends SearchDelegate {
-  @override
-  List<Widget> buildActions(BuildContext context) {
-    return [
-      IconButton(
-        icon: const Icon(Icons.clear),
-        onPressed: () {
-          query = ''; // Aktion zum Löschen der Suchanfrage
-        },
-      ),
-    ];
-  }
-
-  @override
-  Widget buildLeading(BuildContext context) {
-    return IconButton(
-      icon: const Icon(Icons.arrow_back),
-      onPressed: () {
-        Navigator.pop(context, null); // Schließt die Suchansicht und gibt `null` zurück
-      },
-    );
-  }
-
-  @override
-  Widget buildResults(BuildContext context) {
-    if (query.isNotEmpty) {
-      return _buildSearchResults(query); // Zeigt Suchergebnisse basierend auf der aktuellen Anfrage an
-    } else {
-      return Container(); // Leerer Container, wenn die Suchanfrage leer ist
-    }
-  }
-
-  @override
-  Widget buildSuggestions(BuildContext context) {
-    if (query.isNotEmpty) {
-      return _buildSearchResults(query); // Zeigt Suchergebnisse basierend auf der aktuellen Anfrage an
-    } else {
-      return Container(); // Leerer Container, wenn die Suchanfrage leer ist
-    }
-  }
-
   // Funktion zum Aufbau der Suchergebnisse
+
   Widget _buildSearchResults(String query) {
     return FutureBuilder(
       future: FirebaseFirestore.instance.collection('Users').get(),
@@ -132,5 +121,45 @@ class CustomSearchDelegate extends SearchDelegate {
         }
       },
     );
+  }
+
+  @override
+  List<Widget> buildActions(BuildContext context) {
+    return [
+      IconButton(
+        icon: const Icon(Icons.clear),
+        onPressed: () {
+          query = ''; // Aktion zum Löschen der Suchanfrage
+        },
+      ),
+    ];
+  }
+
+  @override
+  Widget buildLeading(BuildContext context) {
+    return IconButton(
+      icon: const Icon(Icons.arrow_back),
+      onPressed: () {
+        Navigator.pop(context, null); // Schließt die Suchansicht und gibt `null` zurück
+      },
+    );
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    if (query.isNotEmpty) {
+      return _buildSearchResults(query); // Zeigt Suchergebnisse basierend auf der aktuellen Anfrage an
+    } else {
+      return Container(); // Leerer Container, wenn die Suchanfrage leer ist
+    }
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    if (query.isNotEmpty) {
+      return _buildSearchResults(query); // Zeigt Suchergebnisse basierend auf der aktuellen Anfrage an
+    } else {
+      return Container(); // Leerer Container, wenn die Suchanfrage leer ist
+    }
   }
 }
